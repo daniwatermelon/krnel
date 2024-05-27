8855400ac62126c29f4eebd65b8441dda36ebba7
Dani <a20300705@ceti.mx>
2024-05-27 16:56:02 -0600

    Con la parte de --host

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import  {VitePWA} from "vite-plugin-pwa";
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),VitePWA(
+{
+  manifest:{
+name: "KrnelPWA",
+short_name:"Krnel",
+theme_color:"#AC21C",
+icons: [
+  {
+    src: '/icons/iconpwa.png',
+    sizes: '512x512',
+    type: 'image/png',
+  }
+]
+  },
+  registerType: 'autoUpdate',
+  devOptions:{enabled: true}
+}
+
+  )],
 })
