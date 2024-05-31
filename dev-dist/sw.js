162388c7742a61b9def2bfee1c43df14a5035839
Dani <a20300705@ceti.mx>
2024-05-31 13:32:45 -0600

    version con auth de google

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.umif1245rv"
+    "revision": "0.794ojqreupg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- src/componentes/LoginForm.jsx
+++ src/componentes/LoginForm.jsx
@@ -1,50 +1,112 @@
-
 import React, { useState } from 'react';
-import { signInWithEmailAndPassword } from 'firebase/auth';
-import { auth } from '../firebaseConfig'; // Asegúrate de importar auth desde tu archivo de configuración
+import { db, auth } from '../firebaseConfig'; 
+import { collection, getDocs, query, where, addDoc } from "firebase/firestore";
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import './LoginForm.css';
 
 const LoginForm = () => {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
     const [error, setError] = useState(null);
+    const provider = new GoogleAuthProvider();
+
+    const handleGoogleSignIn = async () => {
+        try {
+            const result = await signInWithPopup(auth, provider);
+            const user = result.user;
+            console.log('Google sign-in successful:', user);
+
+            // Verifica si el usuario ya existe en tu base de datos
+            const usersRef = collection(db, 'usuario');
+            const q = query(usersRef, where("email", "==", user.email));
+            const querySnapshot = await getDocs(q);
+
+            if (querySnapshot.empty) {
+                // Si el usuario no existe, crea un nuevo documento con su información
+                await addDoc(usersRef, {
+                    email: user.email,
+                    username: user.displayName,
+                    stars: 0,
+                    nivel: "B1",
+                    // Agrega cualquier otra información que desees almacenar sobre el usuario
+                });
+                console.log('User created in Firestore:', user.email);
+            } else {
+                console.log('User already exists in Firestore:', user.email);
+            }
+        } catch (error) {
+            setError(error.message);
+        }
+    };
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        
+        setError(null);
+
         try {
-            const userCredential = await signInWithEmailAndPassword(auth, username, password);
-            console.log('Login successful:', userCredential.user);
-            // aqui va el inicio de sesion exitoso
+            // Consulta para encontrar el documento con el username
+            const q = query(collection(db, "usuario"), where("username", "==", username));
+            const querySnapshot = await getDocs(q);
+
+            if (querySnapshot.empty) {
+                setError("Username not found");
+                return;
+            }
+
+            // Supongamos que solo hay un documento que coincide con el username
+            const userDoc = querySnapshot.docs[0];
+            const userData = userDoc.data();
+
+            if (userData.password === password) {
+                console.log('Login successful:', userData);
+                // Aquí puedes manejar lo que sucede después de un inicio de sesión exitoso
+            } else {
+                setError("Incorrect password");
+            }
         } catch (error) {
             setError(error.message);
         }
     };
 
     return (
-        <form className='card card-body' onSubmit={handleSubmit}>
-            <div>
-                <label htmlFor="username">Username</label>
-                <input
-                    type="text"
-                    id="username"
-                    value={username}
-                    onChange={(e) => setUsername(e.target.value)}
-                    required
-                />
+        <div className="login-container">
+            <div className="login-form">
+                <h1>Krnel</h1>
+                <h2>Iniciar sesión</h2>
+                <form onSubmit={handleSubmit}>
+                    <div>
+                        <label htmlFor="username">Nombre de usuario</label>
+                        <input 
+                            type="username" 
+                            value={username} 
+                            onChange={(e) => setUsername(e.target.value)} 
+                            required
+                        />
+                    </div>
+                    <div>
+                        <label>Contraseña</label>
+                        <input 
+                            type="password" 
+                            id="password"
+                            value={password} 
+                            onChange={(e) => setPassword(e.target.value)} 
+                        />
+                    </div>
+                    {error && <div style={{ color: 'red', fontFamily: "Figtree" }}>{error}</div>}
+                    <div className="button-container">
+                        <button type="submit">Entrar</button>
+                        <button type="button" onClick={handleGoogleSignIn}>Iniciar sesión con Google</button>
+                    </div>
+                    
+                </form>
+                
+                
+                <a href="/forgot-password">¿Olvidaste tu contraseña?</a>
             </div>
-            <div>
-                <label htmlFor="password">Password</label>
-                <input
-                    type="password"
-                    id="password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    required
-                />
+            <div className="register-section">
+                <p>¿Todavía no tienes cuenta? <a href="/register">¡Regístrate!</a></p>
             </div>
-            {error && <div style={{ color: 'red' }}>{error}</div>}
-            <button type="submit">Login</button>
-        </form>
+        </div>
     );
 };
 
