541de9825b9b34db47c53bd49af8a0e16183f480
Daniel Atilano Romero <a20300705@ceti.mx>
2024-06-14 12:30:49 -0600

    ultimo de recoverpass

--- src/componentes/sendPasswordResetEmail.js
+++ src/componentes/sendPasswordResetEmail.js
@@ -1,25 +1,18 @@
-import { db, auth } from '../firebaseConfig'; 
-
 const sendPasswordResetEmail = async (email) => {
-  try {
-    // Verifica si el correo electrónico existe en la base de datos Firestore
-    const userRef = await db.collection('usuario').where('email', '==', email).get();
-    if (userRef.empty) {
-      throw new Error('El correo electrónico no está registrado en la base de datos.');
-    }
-    const userId = userRef.docs[0].id;
-    
-    // Envía el correo de restablecimiento de contraseña utilizando el correo electrónico proporcionado
-    await auth.sendPasswordResetEmail(email);
-    
-    // Registro exitoso
-    console.log('Email de restablecimiento de contraseña enviado');
-    return true;
-  } catch (error) {
-    // Error al enviar el email de restablecimiento de contraseña
-    console.error('Error al enviar el email de restablecimiento de contraseña:', error);
-    return false;
+  const response = await fetch('https://localhost:5000/api/request-reset-password', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ email })
+  });
+
+  const data = await response.json();
+  if (response.status !== 200) {
+    throw new Error(data.message || 'Error al enviar el correo');
   }
+
+  return data.message;
 };
 
 export default sendPasswordResetEmail;
