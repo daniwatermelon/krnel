3dfefa31c47e200129451d3b59ec218a84d8f87d
daniwatermelon <a20300705@ceti.mx>
2024-07-03 21:48:48 -0600

    first commit

--- src/componentes/PassForm.jsx
+++ src/componentes/PassForm.jsx
@@ -1,17 +1,62 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { db } from '../firebaseConfig';
-import { collection, getDocs, query, where } from "firebase/firestore";
+import { collection, getDocs, query, where, runTransaction, doc } from "firebase/firestore";
 import axios from 'axios';
-import './PassForm.css'; // Asegúrate de tener el archivo de estilos si es necesario
+import './PassForm.css'; 
+import { encryptPassword } from '../encryptPassword.js'; // Importa la función de encriptación
+
 
 const PassForm = () => {
   const navigate = useNavigate();
+  const [securityMessage,setSecurityMessage] = useState('');
+  const [username, setUsername] = useState('');
   const [email, setEmail] = useState('');
   const [inputCode, setInputCode] = useState('');
+  const [newPassword, setNewPassword] = useState('');
+  const [newPasswordAgain, setNewPasswordAgain] = useState('');
   const [error, setError] = useState(null);
   const [success, setSuccess] = useState(false);
+  const [isCodeVerified, setIsCodeVerified] = useState(false);
   const [timeLeft, setTimeLeft] = useState(600); // 10 minutes in seconds
+  const [isPasswordChanged, setIsPasswordChanged] = useState(false);
+  const getPasswordSecurity = () => {
+    if (newPassword.length < 8) return 'Débil';
+    if (newPassword.includes(username)) return 'Débil';
+
+    const hasUpperCase = /[A-Z]/.test(newPassword);
+    const hasLowerCase = /[a-z]/.test(newPassword);
+    const hasNumber = /\d/.test(newPassword);
+    const hasSpecialChar = /[-.,_]/.test(newPassword);
+    const typesCount = [hasUpperCase, hasLowerCase, hasNumber, hasSpecialChar].filter(Boolean).length;
+    
+    if (newPassword.length === 8 && typesCount >= 2) return 'Buena';
+    if (newPassword.length >= 8 && newPassword.length <= 12 && typesCount > 2) return 'Fuerte';
+
+    
+};
+
+const getUserName = async () => {
+  try {
+    const userRef = collection(db, 'usuario');
+    const q = query(userRef, where("email", "==", email));
+    const querySnapshot = await getDocs(q);
+
+    if (!querySnapshot.empty) {
+      querySnapshot.forEach((doc) => {
+        setUsername(doc.data().username); // Asumiendo que el campo de nombre se llama 'name'
+      });
+      setError(null);
+    } else {
+      setError('Email no encontrado');
+      setName('');
+    }
+  } catch (error) {
+    console.error('Error obteniendo el nombre:', error);
+    setError('Error obteniendo el nombre');
+    setName('');
+  }
+};
 
   useEffect(() => {
     let timer;
@@ -32,20 +77,22 @@ const PassForm = () => {
   }, [success]);
 
   const sendEmail = async () => {
-    try {
-      const response = await axios.post('http://localhost:3001/send-email', {
-        to: email,
-        subject: 'Aquí está el código para recuperar tu contraseña ' + getCurrentTime(),
-      });
-      console.log('Email sent:', response.data);
-      setSuccess(true);
-      setError(null);
-      setTimeLeft(600); // Reset the timer whenever an email is sent
-    } catch (error) {
-      console.error('Error sending email:', error);
-      setError('Error sending email');
-      setSuccess(false);
-    }
+
+      try {
+        const response = await axios.post('http://localhost:3001/send-email', {
+          to: email,
+          subject: 'Aquí está el código para recuperar tu contraseña ' + getCurrentTime(),
+        });
+        console.log('Email sent:', response.data);
+        setSuccess(true);
+        setError(null);
+        setIsPasswordChanged(false);
+        setTimeLeft(600); // Reset the timer whenever an email is sent
+      } catch (error) {
+        console.error('Error sending email:', error);
+        setError('Error sending email');
+        setSuccess(false);
+      }
   };
 
   function getCurrentTime() {
@@ -62,7 +109,8 @@ const PassForm = () => {
       const response = await axios.post('http://localhost:3001/verify-code', { code: inputCode });
       if (response.status === 200) {
         console.log('Code verified successfully');
-        // Navegar a la página de cambio de contraseña o realizar otra acción
+        getUserName()
+        setIsCodeVerified(true); // Set the code verification state to true
       } else {
         setError('Invalid code');
       }
@@ -94,19 +142,112 @@ const PassForm = () => {
     }
   };
 
+  const handlePasswordChange = (e) => {
+    setNewPassword(e.target.value);
+    const securityLevel = getPasswordSecurity(e.target.value, username);
+    setSecurityMessage(`Nivel de seguridad de la contraseña: ${securityLevel}`);
+};
+
+
+
+
+  const handleChangePassword = async (e) => {
+    e.preventDefault();
+    if (newPassword !== newPasswordAgain) {
+      setError('Las contraseñas no coinciden.');
+      return;
+    }
+    try {
+      const userRef = collection(db, 'usuario');
+      const q = query(userRef, where("email", "==", email));
+      const querySnapshot = await getDocs(q);
+
+      if (!querySnapshot.empty) {
+        querySnapshot.forEach(async (document) => {
+          await runTransaction(db, async (transaction) => {
+            const docRef = doc(db, 'usuario', document.id);
+            const docSnapshot = await transaction.get(docRef);
+            if (!docSnapshot.exists()) {
+              throw "El documento no existe!";
+            }
+            transaction.update(docRef, { password: encryptPassword(newPassword) });
+            setIsPasswordChanged(true);
+          });
+          console.log("Nombre de usuario actualizado con éxito");
+        });
+        setError(null);
+      } else {
+        console.error('Email no encontrado');
+        setError('Email no encontrado');
+      }
+    } catch (error) {
+      console.error('Error actualizando el nombre de usuario:', error);
+      setError('Error actualizando el nombre de usuario');
+    }
+  };
+
   const formatTime = (seconds) => {
     const minutes = Math.floor(seconds / 60);
     const secs = seconds % 60;
     return `${minutes.toString().padStart(2, '0')}:${secs.toString().padStart(2, '0')}`;
   };
+  const passwordSecurity = getPasswordSecurity(newPassword, username);
 
+  let messageColor;
+  switch (passwordSecurity) {
+      case 'Buena':
+          messageColor = 'green';
+          break;
+      case 'Fuerte':
+          messageColor = 'blue';
+          break;
+      default:
+          messageColor = 'red';
+  }
   return (
     <div className="login-container">
       <div className="login-form">
         <h1>Krnel</h1>
         <h2>Recuperar contraseña</h2>
         
-        {success ? (
+        {isPasswordChanged ? (
+          <div>
+            <h3 style={{ color: 'green' }}>Contraseña cambiada con éxito</h3>
+            <button onClick={() => navigate('/')}>Volver al inicio</button>
+          </div>
+        ) :
+        isCodeVerified ? (
+          
+          <form onSubmit={handleChangePassword}>
+            <div>
+                <label htmlFor='new-password' style={{ color: 'black' }}>Ingresa tu nueva contraseña:</label>
+                <input 
+                  type='password'
+                  value={newPassword}
+                  onChange={handlePasswordChange}
+                />
+            </div>
+            
+            <div>
+                <label htmlFor='new-password-confirm' style={{ color: 'black' }}>Ingresa tu nueva contraseña:</label>
+                <input 
+                  type='password'
+                  value={newPasswordAgain}
+                  onChange={(e) => setNewPasswordAgain(e.target.value)}
+                />
+            </div>
+            
+            <div className='button-container'>
+              <button type='submit'>Cambiar Contraseña</button>
+              <button type='button' onClick={() => navigate(-1)}>Cancelar</button>
+            </div>
+            <div style={{ color: messageColor }}>
+                        {securityMessage}
+                    </div>
+            {error && <div style={{ color: 'red', fontFamily: 'Figtree' }}>{error}</div>}
+            
+          </form>
+        ) : success ? (
           <form onSubmit={handleCodeSubmit}>
             <label htmlFor='code' style={{ color: 'black' }}>Ingresa el código enviado a tu email:</label>
             <input 
@@ -117,13 +258,10 @@ const PassForm = () => {
             <div className='button-container'>
               <button type='submit'>Verificar Código</button>
               <button type='button' onClick={() => navigate(-1)}>Regresar</button>
-
             </div>
             <div className='button-container'>
-            <button style={{color: 'white', minWidth: '300'}}type= 'button' onClick={handleCodeSubmit}>Volver a enviar código</button>
-
+              <button style={{color: 'white', minWidth: '300'}} type= 'button' onClick={sendEmail}>Volver a enviar código</button>
             </div>
-
             {error && <div style={{ color: 'red', fontFamily: 'Figtree' }}>{error}</div>}
             <div style={{ color: 'blue', marginTop: '10px' }}>Tiempo restante: {formatTime(timeLeft)}</div>
           </form>
